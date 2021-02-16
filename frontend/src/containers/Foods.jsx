fcfeaf7f4b6c62f1417a19ba0b1bd377b8b4ef9f
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-02-16 18:41:25 +0900

    foodReducer定義

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,14 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+//reducers
+import {
+  initialState as foodInitialState,
+  foodsActionTypes,
+  foodReducer,
+  foodsReducer,
+} from '../reducers/foods';
+
+// apis
+import { fetchFoods } from '../apis/foods';
+
+//constants
+import { REQUEST_STATE } from '../constants';
 
 export const Foods = ({
   match
-}) => {
+  }) => {
+    const [foodState, dispatch] = useReducer(foodsReducer, foodInitialState);
+    useEffect(() => {
+      dispatch({ type: foodsActionTypes.FETCHING });
+      fetchFoods(match.params.restaurantsId)
+      .then((data) => {
+        dispatch({
+          type: foodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
+    },[])
+
   return (
     <Fragment>
-      フード一覧
-      <p>
-        restaurantsIdは {match.params.restaurantsId} です
-      </p>
+      {
+        foodState.fetchState === REQUEST_STATE.LOADING?
+        <Fragment>
+          <p>
+            ロード中...
+          </p>
+        </Fragment>
+        :
+        foodState.foodsList.map(food =>
+          <div key={food.id}>
+            {food.name}
+          </div>
+        )
+      }
     </Fragment>
   )
 }
